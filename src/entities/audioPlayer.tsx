75a3ae3f525d3dcb2ce1c70d3fb5f8e698644ca7
Aivanii <aivanellok@gmail.com>
2025-08-20 11:08:08 +0700

    updated styles for buttons

--- src/entities/audioPlayer.tsx
+++ src/entities/audioPlayer.tsx
@@ -1,7 +1,7 @@
 const AudioPlayer = () => {
   return (
-    <div className="w-full max-w-4xl p-6 flex justify-center items-center flex-col border-standart-border border-1 rounded-md shadow-standart bg-entity-bg 
-    entity">
+    <main className="w-full max-w-4xl p-6 flex justify-center items-center flex-col border-standart-border border-1 rounded-md shadow-standart bg-entity-bg">
+      
       <div className="flex justify-between items-center">
         <div className="inline-block">
           <img
@@ -10,15 +10,37 @@ const AudioPlayer = () => {
             alt="alt"
           />
         </div>
-        <div>
-
-            <button>&lt;</button>
-            <button>||</button>
-            <button>&gt;</button>
-
+        <div className="inline-flex gap-6 items-center">
+          <button className="aspect-square h-12">
+            <img
+              className="invert-100"
+              width="32"
+              height="32"
+              src="https://img.icons8.com/ios/32/left--v1.png"
+              alt="left--v1"
+            />
+          </button>
+          <button className="aspect-square h-16">
+            <img
+              className="invert-100"
+              width="32"
+              height="32"
+              src="https://img.icons8.com/puffy/32/play.png"
+              alt="play"
+            />
+          </button>
+          <button className="aspect-square h-12">
+            <img
+              className="invert-100"
+              width="32"
+              height="32"
+              src="https://img.icons8.com/ios/32/right--v1.png"
+              alt="right--v1"
+            />
+          </button>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
