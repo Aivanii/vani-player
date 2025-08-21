61daa794bb4959a85cbebf499fdeb09556a5cc51
Aivanii <aivanellok@gmail.com>
2025-08-21 13:19:51 +0700

    updated typescript syntax

--- src/entities/audioPlayer/tools/changePlayedTimeByUser.ts
+++ src/entities/audioPlayer/tools/changePlayedTimeByUser.ts
@@ -1,11 +1,19 @@
-const changePlayedTimeByUser = (
+interface ChangePlayedTimeByUser {
+  (
+    progressAudioBarStatic: HTMLDivElement,
+    audioDurationMS: number,
+    audio: HTMLAudioElement
+  ): ((event: { clientX: number }) => void) | undefined;
+}
+
+const changePlayedTimeByUser: ChangePlayedTimeByUser = (
   progressAudioBarStatic: HTMLDivElement,
   audioDurationMS: number,
   audio: HTMLAudioElement
 ) => {
   if (!progressAudioBarStatic || !audio || !audioDurationMS) return undefined;
 
-  const handleClick = (event) => {
+  const handleClick = (event: { clientX: number; }) => {
     const rect = progressAudioBarStatic.getBoundingClientRect();
     const clickX = event.clientX - rect.left;
     //[0-1]
