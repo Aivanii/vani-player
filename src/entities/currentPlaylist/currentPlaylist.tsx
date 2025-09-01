e2110cfc8cdecfa3869b5d5caa1d79fb80187e43
Aivanii <aivanellok@gmail.com>
2025-09-01 10:36:46 +0700

    fixed typo in styles

--- src/entities/currentPlaylist/currentPlaylist.tsx
+++ src/entities/currentPlaylist/currentPlaylist.tsx
@@ -24,7 +24,7 @@ const CurrentPlaylist = ({
   }, [setPlaylist]);
 
   return (
-    <aside className="relative block border-2 w-full h-full max-w-120 shadow-standart bg-entity-bg border-standart-border rounded-4xl p-4">
+    <aside className="relative block border-1 w-full h-full max-w-120 shadow-standart bg-entity-bg border-standart-border rounded-4xl p-4">
       <h2 className="font-bold text-2xl text-center truncate">Your playlist</h2>
       <div className="pt-4">
         <ul className="flex flex-col gap-4">
