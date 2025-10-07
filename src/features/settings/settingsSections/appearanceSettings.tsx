898bb28bcf794bb37f1565f639b4a86506141ded
Aivanii <aivanellok@gmail.com>
2025-10-07 14:48:43 +0700

    updated styles for blur range input

--- src/features/settings/settingsSections/appearanceSettings.tsx
+++ src/features/settings/settingsSections/appearanceSettings.tsx
@@ -10,9 +10,15 @@ const AppearanceSettings = observer(() => {
         <span className="text-important text-3xl">Appearance</span>
       </div>
       <div className="flex flex-row flex-wrap gap-20">
-        <div>
-          <label htmlFor="blurInput">Blur:</label>
+        <div className="grid grid-cols-2 gap-2">
+          <label
+            htmlFor="blurInput"
+            className="col-start-1 text-center font-bold"
+          >
+            Blur
+          </label>
           <input
+            className="accent-important col-start-1 h-4 cursor-pointer appearance-none"
             id="blurInput"
             name="blurInput"
             value={activeBlur}
@@ -21,7 +27,13 @@ const AppearanceSettings = observer(() => {
             type="range"
             onChange={(event) => setActiveBlur(event.target.value)}
           />
-          <span>px</span>
+          <span className="col-start-2">{activeBlur}px</span>
+          <button
+            onClick={() => setActiveBlur("8")}
+            className="col-start-1 px-8 py-2"
+          >
+            reset
+          </button>
         </div>
       </div>
     </div>
