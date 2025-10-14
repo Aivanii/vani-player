18c03b87a7fbab39072c356adba7f513b8951be4
Aivanii <aivanellok@gmail.com>
2025-10-14 16:32:21 +0700

    fixed width sizes for vertical song recs

--- src/widgets/recommendations/recommendations.tsx
+++ src/widgets/recommendations/recommendations.tsx
@@ -15,12 +15,16 @@ const Recommendations = observer(() => {
 
   return (
     <div className="shadow-standart inner-glow bg-entity-bg border-standart-border backdrop-blur-dynamic rounded-dynamic border-size-dynamic relative z-0 mx-auto mb-2 block h-max w-dvw self-stretch px-2 py-6 sm:w-full sm:px-6 md:max-h-[540px]">
-      <div className="py-2">
-        <h3 className="truncate text-2xl font-bold">Recommended for you</h3>
+      <div className="pb-4">
+        <h3
+          className={`truncate text-2xl font-bold ${recsStyle === "horizontal" ? "" : "ml-6"}`}
+        >
+          Recommended for you
+        </h3>
       </div>
       <div className="mx-auto mt-2 mb-2 flex max-h-152 items-start justify-center overflow-y-auto">
         <div
-          className={`mx-auto my-4 flex w-full justify-center gap-4 ${recsStyle === "horizontal" ? "max-h-[430px] flex-row flex-wrap" : "flex-col flex-nowrap"}`}
+          className={`mx-auto my-4 flex w-full justify-center gap-4 ${recsStyle === "horizontal" ? "max-h-[430px] flex-row flex-wrap" : "flex-col flex-nowrap md:w-xl lg:w-3xl"}`}
         >
           {recommendations.map((song) => {
             return (
