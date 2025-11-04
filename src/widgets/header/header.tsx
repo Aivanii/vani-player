32994460cbe2fce65ee6cd72eb267ebae072193b
Aivanii <aivanellok@gmail.com>
2025-11-04 12:22:04 +0700

    improved SEO

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -6,37 +6,11 @@ const Header = () => {
       <header className="border-standart-border shadow-standart bg-entity-bg border-size-dynamic relative z-100 flex items-center justify-start px-6 py-4">
         <a href="/" className="pointer">
           <div className="flex items-center gap-3">
-            <span className="rounded-dynamic flex h-8 w-8 items-center justify-center bg-gradient-to-tr from-[#41C7AD] to-[#9A868D]">
-              <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
-                <circle
-                  cx="14"
-                  cy="14"
-                  r="13"
-                  fill="url(#pulse-logo-gradient)"
-                ></circle>
-                <path
-                  d="M9 19v-7l8-2v7"
-                  stroke="#FFD700"
-                  strokeWidth="2.1"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-                <defs>
-                  <linearGradient
-                    id="pulse-logo-gradient"
-                    x1="0"
-                    y1="0"
-                    x2="28"
-                    y2="28"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopColor="#43E97B"></stop>
-                    <stop offset="0.5" stopColor="#38F9D7"></stop>
-                    <stop offset="1" stopColor="#FF6A88"></stop>
-                  </linearGradient>
-                </defs>
-              </svg>
-            </span>
+            <img
+              src="./favicon.svg"
+              alt="site logo"
+              className="border-size-dynamic border-standart-border aspect-square w-10 rounded-full"
+            />
             <span className="text-color-primary hidden text-2xl font-bold tracking-tight select-none sm:block">
               vani-player
             </span>
