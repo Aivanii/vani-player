8ffcd3230cef830cdd56585e96d7b7f4535cf803
Aivanii <aivanellok@gmail.com>
2026-01-12 11:18:16 +0700

    added tests for formatTime util

---
+++ src/__tests__/utils/formatTime.test.ts
@@ -0,0 +1,8 @@
+import { formatTime } from "../../utils/audio/formatTime";
+
+describe("formatTime", () => {
+  it("works with correct params", () => {
+    expect(formatTime(200000)).toBe("03:20");
+    expect(formatTime(99999999)).toBe("99:59+");
+  });
+});
